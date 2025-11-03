33efc2213c57b7395859fe3ae2bd865a1659d70f
SurajR2 <surajrasaili190@gmail.com>
2025-11-03 21:08:50 +0545

    initialization

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("Server is working");
+});
+
+export default app;
